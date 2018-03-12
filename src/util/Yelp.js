7e3d40402c8896e3332c26d573ae0a827b1f6173
cdrake118 <cdrake@gmail.com>
2018-03-12 16:47:39 -0400

    bug update

--- src/util/Yelp.js
+++ src/util/Yelp.js
@@ -11,7 +11,7 @@ let Yelp = {
         return response.json();
     }).then(function(jsonResponse){
         if(jsonResponse.businesses){
-           return jsonResponses.businesses.map(
+           return jsonResponse.businesses.map(
                 function(business){
                     id: business.id,
                     imageSrc: business.image_url,
