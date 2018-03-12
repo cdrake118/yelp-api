daa9c06b4421e4d7a6cdc3754b66d681c5acd850
cdrake118 <cdrake@gmail.com>
2018-03-12 16:40:28 -0400

    add return

--- src/util/Yelp.js
+++ src/util/Yelp.js
@@ -1,17 +1,17 @@
-const apiKey = 'iIaWei1JWFPv-Jx8lNiiyfqLwV3JXUsioT3PcBQzzgL6UGgZUwNyHbcwyHnpUGhQiI_R_ccAj7kNYXKx-Ocvd9CiLXqntW86_C1ozbjRvYu7bmonJ-wx125besymWnYx';
+const apiKey = "iIaWei1JWFPv-Jx8lNiiyfqLwV3JXUsioT3PcBQzzgL6UGgZUwNyHbcwyHnpUGhQiI_R_ccAj7kNYXKx-Ocvd9CiLXqntW86_C1ozbjRvYu7bmonJ-wx125besymWnYx";
 let Yelp = {
     search(term, location, sortBy){
         return fetch(`https://cors-anywhere.herokuapp.com/https://api.yelp.com/v3/businesses/search?term=${term}&location=${location}&sort_by=${sortBy}`,
        {
            headers: {
               Authorization: `Bearer ${apiKey}`
-           }
+        }
        } 
     ).then(function(response){
         return response.json();
     }).then(function(jsonResponse){
         if(jsonResponse.businesses){
-            jsonResponses.businesses.map(
+           return jsonResponses.businesses.map(
                 function(business){
                     id: business.id,
                     imageSrc: business.image_url,
